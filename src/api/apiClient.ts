a28007c42a4d939cdb66868528050b1f71a299c9
NeelContractor <neelcontractor7@gmail.com>
2026-04-19 16:23:17 +0530

    fixed url issue

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -13,8 +13,8 @@ type ApiOptions = {
 // const BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "";
 // const BASE_URL = "";
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
-    ? `${import.meta.env.VITE_API_BASE_URL}/api/v1`
-    : "/api/v1";
+    ? `${import.meta.env.VITE_API_BASE_URL}`
+    : "";
 
 export class ApiError extends Error {
     status: number;
